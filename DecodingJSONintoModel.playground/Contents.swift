41788897112b6786fd9be035211af0e8e44eea79
mi81ma <mi81ma@gmail.com>
2018-12-05 16:19:12 +0800

    2nd time practice

--- DecodingJSONintoModel.playground/Contents.swift
+++ DecodingJSONintoModel.playground/Contents.swift
@@ -1,41 +1,78 @@
 
 import UIKit
 
-struct Customer : Decodable {
+//struct Customer : Codable {
+//    var firstName :String
+//    var lastName :String
+//    var age :Int
+//
+//    /*
+//    private enum CodingKeys : String, CodingKey {
+//        case firstName
+//        case lastName
+//        case age
+//    }
+//
+//
+//    init(from decoder :Decoder) throws {
+//
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.firstName = try container.decode(String.self, forKey: .firstName)
+//        self.lastName = try container.decode(String.self, forKey: .lastName)
+//        self.age = try container.decode(Int.self, forKey: .age)
+//    }
+// */
+//}
+
+
+
+//let json = """
+//
+//{
+//    "firstName" : "Jonhn",
+//    "lastName" : "Doe",
+//    "age" : 34
+//}
+//
+//""".data(using: .utf8)!
+//
+//
+//let customer = try! JSONDecoder().decode(Customer.self, from: json)
+//print(customer)
+//
+
+
+
+// ************Encoding********************
+
+struct Customer : Codable {
     var firstName :String
     var lastName :String
     var age :Int
 
-    /*
-    private enum CodingKeys : String, CodingKey {
-        case firstName
-        case lastName
-        case age
-    }
 
-
-    init(from decoder :Decoder) throws {
-
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.firstName = try container.decode(String.self, forKey: .firstName)
-        self.lastName = try container.decode(String.self, forKey: .lastName)
-        self.age = try container.decode(Int.self, forKey: .age)
-    } */
+//        private enum CodingKeys : String, CodingKey {
+//            case firstName
+//            case lastName
+//            case age
+//        }
+//
+//
+//    func encode(to encoder :Encoder) throws {
+//
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(self.firstName, forKey: .firstName)
+//        try container.encode(self.lastName, forKey: .lastName)
+//        try container.encode(self.age, forKey: .age)
+//}
 }
 
+let customer = Customer(firstName: "John", lastName: "Doe", age: 45)
+let encodedCustomerJSON = try! JSONEncoder().encode(customer)
 
+print(encodedCustomerJSON)
 
-let json = """
+print(String(data: encodedCustomerJSON, encoding: .utf8))
 
-{
-    "firstName" : "Jonhn",
-    "lastName" : "Doe",
-    "age" : 34
-}
-
-""".data(using: .utf8)!
-
-
-let customer = try! JSONDecoder().decode(Customer.self, from: json)
-print(customer)
-
+let customerJson = try! JSONDecoder().decode(Customer.self, from: encodedCustomerJSON)
+print(customerJson)
